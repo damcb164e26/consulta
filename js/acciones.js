de13fc83b1c6a7aa987eb4677725c33bc26433f8
lorena Alonzo <damcb164e26@gmail.com>
2015-11-18 11:59:18 -0600

    consulta
    aplicacion2

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,23 @@
 // JavaScript Document
 $(document).ready(function(e) {
+	function consulta()
+	{
+		datos="";
+		$.ajax({
+			type: "POST",
+			url: "192.168.1.30/procesar.php",
+			data: datos
+	}).done(function(mensaje){
+		alert(mensaje);
+	});
+}
+			
+			
 document.addEventListener("deviceready",function(){
-	
+	$("#btnconsulta").on ("tap",function(){
+		alert ("Preguntando...");
+		consulta();
+	});
 }); 
 });
 
